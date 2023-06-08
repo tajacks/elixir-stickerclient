4a5aff88889a1c142c5ce1b0c72485c5403f91da
Thomas Jack <tom@tomjack.ca>
2023-06-08 11:15:03 -0400

    real http adapter impl

---
+++ lib/stickerclient/common/http_adapter.ex
@@ -0,0 +1,8 @@
+defmodule StickerClient.HTTPAdapter do
+  @behaviour StickerClient.HTTPAdapterBehaviour
+
+  @impl StickerClient.HTTPAdapterBehaviour
+  def perform_get(url) do
+    Req.get(url, connect_options: [transport_opts: [cacerts: :public_key.cacerts_get()]])
+  end
+end
